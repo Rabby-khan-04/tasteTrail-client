9908afeba3f3749bc6533cd90101cac0f0806df5
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-11 17:53:44 +0600

    Product delete function added

--- src/components/shared/ProductCard/ProductCard.jsx
+++ src/components/shared/ProductCard/ProductCard.jsx
@@ -2,14 +2,45 @@ import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import toast from "react-hot-toast";
 
 const ProductCard = ({ product, action = false }) => {
   const { _id, name, image, category, price, description } = product;
-
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const axiosSecure = useAxiosSecure();
+
+  const { mutateAsync: deleteProduct } = useMutation({
+    mutationFn: () => axiosSecure.delete(`/products/product/${_id}`),
+    onSuccess: (res) => {
+      if (res?.data?.data?.deletedCount > 0) {
+        toast.success("Product deleted successfully!!");
+        queryClient.invalidateQueries(["my-products"]);
+      }
+    },
+  });
 
   const handleUpdate = () => {
     navigate(`/dashboard/update-product/${_id}`);
+  };
+
+  const handleDelete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#FF0000",
+      cancelButtonColor: "#195908",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteProduct();
+      }
+    });
   };
 
   return (
@@ -48,7 +79,10 @@ const ProductCard = ({ product, action = false }) => {
           >
             <FiEdit className="inline-block" />
           </button>
-          <button className="text-xl bg-red-400 text-red-600 block w-full text-center p-4 rounded-md">
+          <button
+            onClick={handleDelete}
+            className="text-xl bg-red-400 text-red-600 block w-full text-center p-4 rounded-md"
+          >
             <MdDelete className="inline-block" />
           </button>
         </div>
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -13,10 +13,14 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import OrderRow from "@/components/Orders/OrderRow";
+import OutlineButton from "@/components/shared/OutlineButton/OutlineButton";
+import { useNavigate } from "react-router-dom";
+import { FaShoppingBag } from "react-icons/fa";
 
 const Orders = () => {
   const { user } = useContext(AuthContext);
   const axiosSecure = useAxiosSecure();
+  const navigate = useNavigate();
 
   const { data: myOrders, isLoading: myOrderLoading } = useQuery({
     queryFn: () =>
@@ -31,33 +35,45 @@ const Orders = () => {
     0
   );
 
-  console.log(totalPrice);
-
   return (
     <div>
       <h2 className="text-6xl font-louize_medium text-primary mb-6">Orders</h2>
-      <Table>
-        <TableHeader>
-          <TableRow className="text-2xl font-maax_medium text-primary">
-            <TableHead>Product</TableHead>
-            <TableHead>Seler</TableHead>
-            <TableHead>Quantity</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead className="text-right">Total</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {myOrders?.data?.data?.map((order) => (
-            <OrderRow key={order._id} order={order} />
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow className="text-2xl font-maax_medium text-primary">
-            <TableCell colSpan={4}>Total</TableCell>
-            <TableCell className="text-right">${totalPrice}</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+      {myOrders?.data?.data?.length === 0 ? (
+        <div className="flex flex-col items-center gap-4 text-center">
+          <h2 className="text-xl font-maax_medium text-primary">
+            You haven{"'"}t placed any order
+          </h2>
+          <OutlineButton
+            text="Shop Now"
+            className="text-primary"
+            onClick={() => navigate("/shop")}
+            Icon={FaShoppingBag}
+          />
+        </div>
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow className="text-2xl font-maax_medium text-primary">
+              <TableHead>Product</TableHead>
+              <TableHead>Seler</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Total</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {myOrders?.data?.data?.map((order) => (
+              <OrderRow key={order._id} order={order} />
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow className="text-2xl font-maax_medium text-primary">
+              <TableCell colSpan={4}>Total</TableCell>
+              <TableCell className="text-right">${totalPrice}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      )}
     </div>
   );
 };
